4aa3ca5542b38b46d9d2367f73608f81074d1d82
Shine <331046212@qq.com>
2025-02-15 17:31:57 +0800

    add  log 类

---
+++ My_Package/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: Shine Wu
+# @Date:   2025-02-15 17:14:20
+# @Last Modified by:   Shine Wu
+# @Last Modified time: 2025-02-15 17:29:54
+
+
+from My_Package.My_Logger import My_Logger
+
+#__all__ = ['My_Logger'，'other class']
+__all__ = ['My_Logger']
+print("111")
--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Py_Small_Script
 This poject is a script about ptyhon
+## utilized third-party libraries
+    tkinter
+    tkinter
 ## Rsa_Theory
 This project is a python script about RSA's theory.
+
+
+
+
